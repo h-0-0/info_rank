ee61a380b0a29866b37ec35a768dcd705d7704bf
h-0-0 <jd18380@bc4login1.bc4.acrc.priv>
2024-10-07 13:34:39 +0100

    .

--- submit_nyu.py
+++ submit_nyu.py
@@ -2,28 +2,29 @@ import slune
 
 if  __name__ == "__main__":
     to_search = {
-        'benchmark': ['nyu_v2_13', 'nyu_v2_40'],
+        'benchmark': ['nyu_v2_13'],
         'model': ['ResNet50', 'ResNet101'],
         'num_epochs': [100],
         'batch_size': [8],
-        'patience': [15],
+        'patience': [5],
+        'optimizer': ['SGD'],
     }
     SimCLR = {
         'est': ['SimCLR'],
-        'learning_rate': [1e-1, 1e-2, 1e-3, 1e-4, 1e-6, 1e-8],
-        'temperature': [10, 1, 0.1], 
+        'learning_rate': [1e-2, 1e-3, 1e-4],
+        'temperature': [1], 
         'output_dim': [2048],
     }
     info_critic = {
         'est': ['info_critic'],
-        'learning_rate': [1e-1, 1e-2, 1e-3, 1e-4, 1e-6, 1e-8],
+        'learning_rate': [1e-1, 1e-2, 1e-3],
         'temperature' : [1],
         'output_dim': [2048],
     }
 
     # Join dictionary
-    to_search.update(SimCLR) 
-    # to_search.update(info_critic)
+    # to_search.update(SimCLR) 
+    to_search.update(info_critic)
 
     grid_info_rank = slune.searchers.SearcherGrid(to_search)
 
