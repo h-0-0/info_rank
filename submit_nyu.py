b2aca24b50b8abb9d3b7882d416b6cd9d9c96db9
h-0-0 <jd18380@bristol.ac.uk>
2024-10-24 14:08:13 +0100

    .

--- submit_nyu.py
+++ submit_nyu.py
@@ -3,28 +3,42 @@ import slune
 if  __name__ == "__main__":
     to_search = {
         'benchmark': ['nyu_v2_13'],
-        'model': ['ResNet50', 'ResNet101'],
-        'num_epochs': [100],
-        'batch_size': [8],
-        'patience': [5],
+        'model':  ['ResNet50', 'ResNet101'], #'FCN50', 'FCN101',
+        'num_epochs': [200],
+        'patience': [15],
         'optimizer': ['SGD'],
+
+        'eval_num_epochs': [200],
     }
     SimCLR = {
         'est': ['SimCLR'],
-        'learning_rate': [1e-2, 1e-3, 1e-4],
+        'learning_rate': [1e-3, 1e-4, 1e-5, 1e-6], #1e-4 only one without immediate failure
         'temperature': [1], 
         'output_dim': [2048],
+        'batch_size': [98],
     }
     info_critic = {
         'est': ['info_critic'],
-        'learning_rate': [1e-1, 1e-2, 1e-3],
+        'learning_rate': [1e-2, 1e-3, 1e-4, 1e-5],
         'temperature' : [1],
         'output_dim': [2048],
+        'batch_size': [98],
+    }
+    supervised = {
+        'est': ['supervised'],
+        'learning_rate': [1e-1, 1e-2, 1e-4, 1e-6, 1e-8],
+        'temperature' : [1],
+        'output_dim': [2048],
+        'batch_size': [48],
     }
 
     # Join dictionary
     # to_search.update(SimCLR) 
-    to_search.update(info_critic)
+    # to_search.update(info_critic)
+    to_search.update(supervised)
+
+    # Trying fix unsup learn again with some more lr's, if it doesn't work then try adam
+    # Then the problem of eval_train
 
     grid_info_rank = slune.searchers.SearcherGrid(to_search)
 
